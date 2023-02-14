aac70ce0e5b49064bbe21c1d8e72ab357881f154
tracer-ics2022 <tracer@njuics.org>
2023-02-14 16:02:18 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:02:18 up 5 days, 18:52,  3 users,  load average: 0.14, 0.14, 0.15

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -35,9 +35,10 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   //int i = 0;
+  
+  *success = true;
   for(int i = 0; i < 32; i++){
     if(strcmp(regs[i], s) == 0){
-      *success = true;
       return cpu.gpr[i];
     }
   }
