7ea05dc9b24971c376387754b193a6770fac00ab
tracer-ics2022 <tracer@njuics.org>
2023-02-19 16:27:52 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:27:52 up 9 days,  3:38,  3 users,  load average: 0.85, 0.72, 0.35

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -30,10 +30,14 @@ enum {
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
+#define testimmI() do { *imm = SEXT(BITS(i, 31, 31), 1) << 11| BITS(i, 30, 20);} while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (SEXT(BITS(i, 7, 7), 1) << 11) | (SEXT(BITS(i, 30, 25), 6) << 5) | (SEXT(BITS(i, 11, 8), 4) << 1); } while(0)
-#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (SEXT(BITS(i, 19, 12), 8) << 12) | (SEXT(BITS(i, 11, 11), 1) << 11) | (SEXT(BITS(i, 30, 20), 1) << 1); } while(0)
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (SEXT(BITS(i, 11, 11), 1) << 11) | (SEXT(BITS(i, 30, 20), 1) << 1); } while(0)
+
+// BITS(i, 31, 31) << 20 | BITS(i, 19, 12) << 12 | BITS(i, 11, 11) << 1| BITS(i, 30, 20) << 1
+
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -41,7 +45,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
   immI();
-  immJ();
+  testimmI();
   *dest = rd;
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
