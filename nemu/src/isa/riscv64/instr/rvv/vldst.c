c2f11cd24288636f6e9648cf424bee508485562f
tracer-ics2022 <tracer@njuics.org>
2023-03-25 00:10:24 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:10:24 up  7:39,  1 user,  load average: 0.17, 0.08, 0.03

--- nemu/src/isa/riscv64/instr/rvv/vldst.c
+++ nemu/src/isa/riscv64/instr/rvv/vldst.c
@@ -1,5 +1,23 @@
 #include <common.h>
 
-
+#include "../local-include/reg.h"
+#include <cpu/cpu.h>
+#include <cpu/ifetch.h>
+#include <cpu/decode.h>
+#include <cpu/inst.h>
 #include "vldst.h"
+
+void vld(Decode *s){
+//这里直接调用decode得了
+    uint32_t i = s->isa.inst.val;
+    int nf = 0, mew = 0, mop = 0, vm = 0, lumop = 0, src2 = 0, src1 = 0, width = 0, dest = 0;
+
+    mop = BITS(i, 27, 26);
+    width = BITS(i, 14, 12);
+
+
+    return;
+}
+
+
 
--- nemu/src/isa/riscv64/instr/rvv/vldst.h
+++ nemu/src/isa/riscv64/instr/rvv/vldst.h
@@ -3,11 +3,8 @@
 #ifndef __RVV_VLDST_H__
 #define __RVV_VLDST_H__
 
-#include "../local-include/reg.h"
-#include <cpu/cpu.h>
-#include <cpu/ifetch.h>
-#include <cpu/decode.h>
-#include <cpu/inst.h>
+void vld(Decode *s);
+
 
 
 #endif
