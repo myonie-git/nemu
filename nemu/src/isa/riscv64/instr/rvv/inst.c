2c5aeb8bf191895b98af3d479dfebd8b8b653125
tracer-ics2022 <tracer@njuics.org>
2023-03-24 09:59:03 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:59:03 up 10:55,  1 user,  load average: 0.30, 0.33, 0.34

--- nemu/src/isa/riscv64/instr/rvv/inst.c
+++ nemu/src/isa/riscv64/instr/rvv/inst.c
@@ -11,18 +11,25 @@ enum{
 };
 
 
-static void decode_operand_rvv_load(Decode *s, int *nf, int *mew, int *vm, int *lumop, int *src1, int *src2, int *width, int *dest, int type){
+static void decode_operand_rvv_load(Decode *s, int *nf, int *mew, int *mop, int *vm, int *lumop, int *src1, int *src2, int *width, int *dest, int type){
+  uint32_t i = s->isa.inst.val;
+  *nf = BITS(i, 31, 29);
+  *mew = BITS(i, 28, 28);
+  *mop = BITS(i, 27, 26);
   
+
+
+  return ;
 }
 
 int decode_exec_rvv_load(Decode *s) {
   //检测是否是rv64v_load_store指令
-  int nf = 0, mew = 0, vm = 0, lumop = 0, src2 = 0, src1 = 0, width = 0, dest = 0;
+  int nf = 0, mew = 0, mop = 0, vm = 0, lumop = 0, src2 = 0, src1 = 0, width = 0, dest = 0;
 
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
-  decode_operand_rvv_load(s, &nf, &mew, &vm, &lumop, &src1, &src2, &width, &dest, concat(TYPE_, type)); \
+  decode_operand_rvv_load(s, &nf, &mew, &mop, &vm, &lumop, &src1, &src2, &width, &dest, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
   
