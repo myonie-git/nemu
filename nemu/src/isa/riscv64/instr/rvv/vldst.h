ba6270e143a3e8a546b23a7973333312230d0aa5
tracer-ics2022 <tracer@njuics.org>
2023-03-24 21:04:33 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:33 up  4:33,  1 user,  load average: 0.27, 0.12, 0.04

--- nemu/src/isa/riscv64/instr/rvv/vldst.h
+++ nemu/src/isa/riscv64/instr/rvv/vldst.h
@@ -1,9 +1,9 @@
-#include <commom.h>
+#include <common.h>
 
 #ifndef __RVV_VLDST_H__
 #define __RVV_VLDST_H__
 
-#include "local-include/reg.h"
+#include "../local-include/reg.h"
 #include <cpu/cpu.h>
 #include <cpu/ifetch.h>
 #include <cpu/decode.h>
