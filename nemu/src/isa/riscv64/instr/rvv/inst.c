ad2e73ec19ec4de0df76fa6112506f34133edf1e
tracer-ics2022 <tracer@njuics.org>
2023-03-24 11:14:14 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:14 up 12:10,  1 user,  load average: 0.35, 0.26, 0.21

--- nemu/src/isa/riscv64/instr/rvv/inst.c
+++ nemu/src/isa/riscv64/instr/rvv/inst.c
@@ -6,9 +6,15 @@
 
 enum{
   TYPE_V, //VLD & VSD
-  TYPE_VS, // VLS VLX VSS VSX
+  TYPE_VS, // VLS VSS
+  TYPE_VX, // VLX VSX
   TYPE_N,
 };
+
+
+#define R(i) gpr(i)
+#define src1R() do { *src1 = R(rs1); } while (0)
+#define src2R() do { *src2 = R(rs2); } while (0)
 
 
 static void decode_operand_rvv_load(Decode *s, int *nf, int *mew, int *mop, int *vm, int *lumop, int *src1, int *src2, int *width, int *dest, int type){
@@ -16,8 +22,16 @@ static void decode_operand_rvv_load(Decode *s, int *nf, int *mew, int *mop, int 
   *nf = BITS(i, 31, 29);
   *mew = BITS(i, 28, 28);
   *mop = BITS(i, 27, 26);
-  
-
+  *vm = BITS(i, 25, 25);
+  *lumop = BITS(i, 24, 20);
+  int rs1 = BITS(i, 19, 15);
+  *width = BITS(i, 14, 12);
+  *dest = BITS(i, 11, 7);
+  switch(type){
+    case TYPE_V : break; //get rs1, vd
+    case TYPE_VS: break; //get rs1, rs2, vd
+    case TYPE_VX: break; //get rs1, vs1, vd
+  }
 
   return ;
 }
