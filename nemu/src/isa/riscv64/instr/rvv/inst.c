ec4a5aed215b8eea1eadaf00007832e933aebdb6
tracer-ics2022 <tracer@njuics.org>
2023-03-27 10:51:10 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:51:10 up 6 min,  1 user,  load average: 0.30, 0.31, 0.19

--- nemu/src/isa/riscv64/instr/rvv/inst.c
+++ nemu/src/isa/riscv64/instr/rvv/inst.c
@@ -6,9 +6,6 @@
 #include "vldst.h"
 
 enum{
-  TYPE_V, //VLD & VSD
-  TYPE_VS, // VLS VSS
-  TYPE_VX, // VLX VSX
   TYPE_N,
 };
 
@@ -16,53 +13,6 @@ enum{
 #define R(i) gpr(i)
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
-
-
-static void decode_operand_rvv_load(Decode *s, int *nf, int *mew, int *mop, int *vm, int *lumop, int *src1, int *src2, int *width, int *dest, int type){
-  uint32_t i = s->isa.inst.val;
-  *nf = BITS(i, 31, 29);
-  *mew = BITS(i, 28, 28);
-  *mop = BITS(i, 27, 26);
-  *vm = BITS(i, 25, 25);
-  *lumop = BITS(i, 24, 20);
-  int rs1 = BITS(i, 19, 15);
-  int rs2 = BITS(i, 24, 20);
-  *width = BITS(i, 14, 12);
-  *dest = BITS(i, 11, 7);
-  switch(type){
-    case TYPE_V : src1R();          break; //get rs1, vd
-    case TYPE_VS: src1R(); src2R(); break; //get rs1, rs2, vd
-    case TYPE_VX: src1R();          break; //get rs1, vs1, vd
-  }
-  return ;
-}
-
-int decode_exec_rvv_load(Decode *s) {
-  //检测是否是rv64v_load_store指令
-  int nf = 0, mew = 0, mop = 0, vm = 0, lumop = 0, src2 = 0, src1 = 0, width = 0, dest = 0;
-
-
-#define INSTPAT_INST(s) ((s)->isa.inst.val)
-#define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
-  decode_operand_rvv_load(s, &nf, &mew, &mop, &vm, &lumop, &src1, &src2, &width, &dest, concat(TYPE_, type)); \
-  __VA_ARGS__ ; \
-}
-  
-  INSTPAT_START(rvv_load);
-  INSTPAT("??????? ????? ????? ??? ????? 00001 11", vld    , V,  vld(s));
-  
-  INSTPAT_END(rvv_load);
-
-  
-  return 0;
-}
-
-
-int decode_exec_rvv_store(Decode *s) {
-  //检测是否是rv64v_load_store指令
-  return 0;
-}
-
 /*
 vld*
 
