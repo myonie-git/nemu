906b45219c331914ddf951b135e3d2de169b44d5
tracer-ics2022 <tracer@njuics.org>
2023-03-28 08:34:32 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:34:32 up  7:20,  1 user,  load average: 0.13, 0.21, 0.18

--- nemu/src/isa/riscv64/instr/rvv/inst.c
+++ nemu/src/isa/riscv64/instr/rvv/inst.c
@@ -11,27 +11,12 @@ enum{
 
 
 #define R(i) gpr(i)
-#define src1R() do { *src1 = R(rs1); } while (0)
-#define src2R() do { *src2 = R(rs2); } while (0)
-/*
-vld*
-
-{reg: [
-  {bits: 7, name: 0x7, attr: 'VL* unit-stride'}, - 0x07
-  {bits: 5, name: 'vd', attr: 'destination of load', type: 2}, 
-  {bits: 3, name: 'width'},
-  {bits: 5, name: 'rs1', attr: 'base address', type: 4},
-  {bits: 5, name: 'lumop'},
-  {bits: 1, name: 'vm'},
-  {bits: 2, name: 'mop'},
-  {bits: 1, name: 'mew'},
-  {bits: 3, name: 'nf'},
-]}
-*/
+#define V(i) vgpr(i)
 
 void vgtm(Decode *s){
   uint32_t i = s->isa.inst.val;
   int rs1 = 0, rs2 = 0, funct3 = 0, dest = 0;
+  funct3 = BITS(i, 14, 12);
   
   return ;
 }
