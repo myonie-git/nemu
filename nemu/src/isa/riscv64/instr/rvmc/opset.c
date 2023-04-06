110bf2856b17c89cf68465edaa9086d83af37847
myonie-git <myonie.0724@gmail.com>
2023-04-06 16:34:12 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:12 up 2 days, 30 min,  1 user,  load average: 0.24, 0.19, 0.26

--- nemu/src/isa/riscv64/instr/rvmc/opset.c
+++ nemu/src/isa/riscv64/instr/rvmc/opset.c
@@ -19,22 +19,47 @@
 #define OPFCF 6
 #define OPSET 7
 
+#define INT_MODE 0
+#define REG_MODE 1
+
+#define R(i) gpr(i)
+#define MX(i) mgpr_x(i)
+#define MY(i) mgpr_y(i)
+
+static void decopde_operand(Decode *s, int *tmp1, int* tmp2, int *dest){
+    uint32_t i = s->isa.inst.val;
+    *dest = BITS(i, 11, 7);
+    int src1 = BITS(i, 19, 15);
+    int src2 = BITS(i, 24, 20);
+    switch(BITS(i, 27, 27)){
+        case INT_MODE: *tmp1 = src1; break;
+        case REG_MODE: *tmp1 = R(src1); break;
+        default: assert(0); break;
+    }
+    switch(BITS(i, 26, 26)){
+        case INT_MODE: *tmp2 = src2; break;
+        case REG_MODE: *tmp2 = R(src2); break;
+        default: assert(0); break;
+    }
+    return ;
+}
+
 int opset_decode_exec(Decode *s){
+    word_t tmp1 = 0, tmp2 = 0;
+    int dest = 0;
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
+    decode_operand(s, &tmp1, &tmp2, &dest);  \
     __VA_ARGS__; \
 }
     INSTPAT_START();
 
-    INSTPAT("0000?? ? ????? ????? 111 ????? 00010 11", mvl   , VSET, );
-    INSTPAT("0001?? ? ????? ????? 111 ????? 00010 11", mgpr  , VSET, );
-    INSTPAT("0010?? ? ????? ????? 111 ????? 00010 11", p     , VSET, );
-    INSTPAT("0011?? ? ????? ????? 111 ????? 00010 11", vstart, VSET, );
-    INSTPAT("0100?? ? ????? ????? 111 ????? 00010 11", vlen  , VSET, );
-
-
-
-
+    INSTPAT("0000?? ? ????? ????? 111 ????? 00010 11", mvl   , VSET, cpu.mvl.xlen = tmp1; cpu.mvl.ylen = tmp2;);
+    INSTPAT("0001?? ? ????? ????? 111 ????? 00010 11", mgpr  , VSET, mgpr_x(dest) = tmp1; mgpr_y(dest) = tmp2;);
+    INSTPAT("0010?? ? ????? ????? 111 ????? 00010 11", p     , VSET, cpu.p = tmp1);
+    INSTPAT("0011?? ? ????? ????? 111 ????? 00010 11", vstart, VSET, cpu.vstart = tmp1);
+    INSTPAT("0100?? ? ????? ????? 111 ????? 00010 11", vlen  , VSET, cpu.vl = tmp1);
+    INSTPAT("0101?? ? ????? ????? 111 ????? 00010 11", mctype , VSET, cpu.mctype = tmp1);
     INSTPAT_END();
     return 0;
 }
