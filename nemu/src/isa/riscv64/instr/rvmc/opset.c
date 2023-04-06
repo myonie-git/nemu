48e2314b79d30bd758c7a8a4e2a2121ba1419d73
myonie-git <myonie.0724@gmail.com>
2023-04-06 15:58:31 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:31 up 1 day, 23:54,  1 user,  load average: 0.51, 0.35, 0.46

--- nemu/src/isa/riscv64/instr/rvmc/opset.c
+++ nemu/src/isa/riscv64/instr/rvmc/opset.c
@@ -4,3 +4,37 @@
 #include <cpu/decode.h>
 #include <cpu/inst.h>
 #include "decode.h"
+
+#define MODE_MCTYPE_WIDTH_8 0
+#define MODE_MCTYPE_WIDTH_16 1
+#define MODE_MCTYPE_WIDTH_32 2
+#define MODE_MCTYPE_WIDTH_64 3
+
+#define OPICC 0
+#define OPFCC 1
+#define OPICV 2
+#define OPICX 3
+#define OPICI 4
+#define OPFCV 5
+#define OPFCF 6
+#define OPSET 7
+
+int opset_decode_exec(Decode *s){
+#define INSTPAT_INST(s) ((s)->isa.inst.val)
+#define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
+    __VA_ARGS__; \
+}
+    INSTPAT_START();
+
+    INSTPAT("0000?? ? ????? ????? 111 ????? 00010 11", mvl   , VSET, );
+    INSTPAT("0001?? ? ????? ????? 111 ????? 00010 11", mgpr  , VSET, );
+    INSTPAT("0010?? ? ????? ????? 111 ????? 00010 11", p     , VSET, );
+    INSTPAT("0011?? ? ????? ????? 111 ????? 00010 11", vstart, VSET, );
+    INSTPAT("0100?? ? ????? ????? 111 ????? 00010 11", vlen  , VSET, );
+
+
+
+
+    INSTPAT_END();
+    return 0;
+}
