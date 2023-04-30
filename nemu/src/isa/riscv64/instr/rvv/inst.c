e294e11a85234ced05d1344d6f070a5ab0c34f9f
myonie-git <myonie.0724@gmail.com>
2023-04-30 18:03:04 +0800

    >  compile NEMU MyonieOs Myonie Linux myonie-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:03:04 up 1 day, 20:44,  1 user,  load average: 0.23, 0.26, 0.26

--- nemu/src/isa/riscv64/instr/rvv/inst.c
+++ nemu/src/isa/riscv64/instr/rvv/inst.c
@@ -67,6 +67,7 @@ void vgtm(Decode *s){
         case MODE_UNSIGNED_MCTYPE_WIDTH_64: V(dest)._64[idx] = (uint64_t)V(rs2)._64[idx] > (uint64_t)tmp ? V(rs2)._64[idx] : tmp;     
       }
       //V(dest)._64[idx] = V(rs2)._64[idx] > tmp ? V(rs2)._64[idx] : tmp;
+      printf("tmp: %d, V2: %d\n", (uint8_t)tmp, (int8_t)V(rs2)._8[idx * 8]);
     }
   }
 
